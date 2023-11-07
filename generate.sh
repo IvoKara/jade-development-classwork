de7852db8aa155890b7d59d3eb0406212d1a1eb5
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-11-07 23:40:26 +0200

    feat: add help and usage for generate script

--- generate.sh
+++ generate.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-SHORT="g:a:"
-LONG="groupId:,group-id:,artifactId:,artifact-id"
+help()
+{
+    echo
+    echo "Simplified version of mvn generate with JADE framework in mind."
+    echo
+    echo "Usage:"
+    echo "  generate.sh -g <group.id> -a <artifact-id>"
+    echo "  generate.sh --groupId <group.id> --artifactId <artifact-id>"
+    echo "  generate.sh --group-id=<group.id> -artifact-id=<artifact-id>"
+    echo
+    echo "Options:"
+    echo "  -g, --groupId, --group-id          Maven's project group id."
+    echo "  -a, --artifactId, --artifact-id    Maven's project artifact id."
+    echo "  -h, --help                         Print this help."
+    echo
+}
+
+SHORT="g:a:h"
+LONG="groupId:,group-id:,artifactId:,artifact-id,help"
 OPTIONS=$(getopt --options $SHORT --long $LONG --name="$0" -- "$@")
 
 if [ $? != "0" ]; then
     echo "Failed to parse options...exiting."
+    help
     exit 1
 fi
 
@@ -21,12 +39,17 @@ while :; do
         ARTIFACT_ID="$2"
         shift 2
         ;;
+    -h | --help)
+        help
+        exit 0
+        ;;
     --)
         shift
         break
         ;;
     *)
         echo "Unexpected option $1"
+        help
         ;;
     esac
 done
