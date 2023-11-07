97a8fa0a4ad5b22634f006bd7549f45e864d54d5
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-11-07 15:32:31 +0200

    Initial: create Maven archetype for scaffolding JADE projects

---
+++ jade-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/AppAgent.java
@@ -0,0 +1,11 @@
+package $package;
+
+import jade.core.Agent;
+
+public class AppAgent extends Agent {
+    
+    @Override
+    protected void setup() {
+        System.out.println("Hello world! My name is " + this.getLocalName() + ".");
+    }
+}
