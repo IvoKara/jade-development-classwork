eeb2365c5e170d00d3b3c284a4baf3bffc7b88a4
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-12-05 22:43:27 +0200

    feat(ex5): add model structures

---
+++ ex5-df-services/src/main/java/restaurant/meals/Ingredient.java
@@ -0,0 +1,42 @@
+package restaurant.meals;
+
+public class Ingredient {
+	private String measure;
+	private String unitOfMeasure;
+	private String name;
+
+	public Ingredient(String name, String measure, String unitOfMeasure) {
+		this.measure = measure;
+		this.unitOfMeasure = unitOfMeasure;
+		this.name = name;
+	}
+
+	public String getMeasure() {
+		return measure;
+	}
+
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+
+	public String getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(String unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return measure + " " + unitOfMeasure + " " + name;
+	}
+}
