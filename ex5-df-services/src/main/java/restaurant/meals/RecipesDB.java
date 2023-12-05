d6dce6f388409c17221e336aca662c0d35c43105
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-12-05 22:43:50 +0200

    feat(ex5): create "db" for recepies

---
+++ ex5-df-services/src/main/java/restaurant/meals/RecipesDB.java
@@ -0,0 +1,41 @@
+package restaurant.meals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RecipesDB {
+    private static ArrayList<CookRecipe> recipes;
+
+    static {
+        recipes = new ArrayList<CookRecipe>(
+                Arrays.asList(
+                        new CookRecipe("Apple Pie"),
+                        new CookRecipe("Chicken Soup"),
+                        new CookRecipe("Green Salad"),
+                        new CookRecipe("Sushi")));
+
+        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>(
+                Arrays.asList(
+                        new Ingredient("frozen apples", "1", "package"),
+                        new Ingredient("sugar", "2/3", "cup"),
+                        new Ingredient("salt", "1", "teaspoon"),
+                        new Ingredient("large eggs", "2", ""),
+                        new Ingredient("butter", "2/3", "cup")));
+
+        ArrayList<String> instructions = new ArrayList<String>(
+                Arrays.asList(
+                        "Preheat oven",
+                        "Mix sugar ...",
+                        "Stir in flour ...",
+                        "Bake"));
+
+        for (CookRecipe recipe : recipes) {
+            recipe.addIngredients(ingredients);
+            recipe.addInstructions(instructions);
+        }
+    }
+
+    public static ArrayList<CookRecipe> getAll() {
+        return recipes;
+    }
+}
