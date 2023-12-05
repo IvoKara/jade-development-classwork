aff027268e28ffd2bd080ecba33c76f76ddc9ce3
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-12-06 00:27:47 +0200

    feat(ex5): add proper behavours and send messages

--- ex5-df-services/src/main/java/com/abt/ex5/agents/RecipeFounderAgent.java
+++ ex5-df-services/src/main/java/com/abt/ex5/agents/RecipeFounderAgent.java
@@ -1,10 +1,17 @@
 package com.abt.ex5.agents;
 
+import com.abt.ex5.meals.CookRecipe;
+import com.abt.ex5.meals.Ingredient;
+import com.abt.ex5.meals.RecipesDB;
+
 import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class RecipeFounderAgent extends Agent {
 
@@ -24,11 +31,36 @@ public class RecipeFounderAgent extends Agent {
             fe.printStackTrace();
         }
 
-        // this.registerDFService();
-    }
+        this.addBehaviour(new SimpleBehaviour() {
+            private final MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 
-    private void registerDFService() {
+            @Override
+            public void action() {
+                doWait();
 
+                ACLMessage message = myAgent.receive(mt);
+
+                if (message == null) {
+                    System.out.println("Recepie name not received!");
+                    this.block();
+                    return;
+                }
+
+                CookRecipe recipe = findRecipe(message.getContent());
+
+                if (recipe == null) {
+                    System.out.println("Sorry no such meal in our menu.");
+                    return;
+                }
+
+                printOutRecipe(recipe);
+            }
+
+            @Override
+            public boolean done() {
+                return true;
+            }
+        });
     }
 
     @Override
@@ -39,6 +71,30 @@ public class RecipeFounderAgent extends Agent {
             DFService.deregister(this);
         } catch (FIPAException fe) {
             fe.printStackTrace();
+        }
+    }
+
+    public CookRecipe findRecipe(String recipeName) {
+
+        return RecipesDB.getAll().stream()
+                .filter(r -> recipeName.toLowerCase().equals(r.getName().toLowerCase()))
+                .findAny()
+                .orElse(null);
+    }
+
+    public void printOutRecipe(CookRecipe recipe) {
+        System.out.println("\n------------------------");
+        System.out.println("\t" + recipe.getName());
+        System.out.println("------------------------");
+
+        System.out.println("\nIngredients:");
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            System.out.println(" " + ingredient);
+        }
+
+        System.out.println("\nInstructions:");
+        for (String instruction : recipe.getInstructions()) {
+            System.out.println(" " + instruction);
         }
     }
 }
